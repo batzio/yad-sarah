b4b41c44484bf09328afec2a910a1341fba457b3
batzio <aniandata1234@gmail.com>
2022-05-01 18:03:44 +0300

    login component

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,34 @@
+export const Login = (props) => {
+
+
+    return (
+        <div dir="rtl">
+            <div className="rectangle"></div>
+            <div className="rectangle2">
+                <img id="pic"
+                    src="https://serviced.co.il/wp-content/uploads/2022/01/%D7%A6%D7%95%D7%A8-%D7%A7%D7%A9%D7%A8-%D7%A9%D7%99%D7%A8%D7%95%D7%AA-%D7%9C%D7%A7%D7%95%D7%97%D7%95%D7%AA-%D7%99%D7%93-%D7%A9%D7%A8%D7%94.png" />
+            </div>
+            <div className="container">
+                <form action="/action_page.php">
+                    <div className="row">
+                        <h2 style={{ textAlign: "center" }}>התחברות </h2>
+                        <div className="center">
+                            <div className="hide-md-lg">
+                            </div>
+                            <input type="text" name="username" placeholder="שם משתמש" required />
+                            <input type="password" name="password" placeholder="קוד" required />
+                            <input type="submit" value="התחברות" />
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div className="bottom-container">
+                <div className="row">
+                    <div className="col">
+                        <a href="#" style={{ color: "#1C6BB4" }} className="btn">שכחת סיסמא?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2y6t3-F4it1totrbjA0ocASyFFV3JnDI",
+  authDomain: "yad-sara.firebaseapp.com",
+  projectId: "yad-sara",
+  storageBucket: "yad-sara.appspot.com",
+  messagingSenderId: "31019839130",
+  appId: "1:31019839130:web:8c2dcd79353c7682e5b923"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
