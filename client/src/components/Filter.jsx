e83ebd210d3871c24f9f879db473f8b18c08893b
Elisheva Kaluzhny <kaluzhny.elisheva@gmail.com>
2022-05-11 23:37:57 +0300

    routing pages

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,39 +1,48 @@
-export const Filter = (props) => {
+export const Search = (props) => {
 
 
-    return (
+ return(
 
 
-
-        <div>
-            <div id="ltr">
-                <div className="rectangle">
-                </div>
-                <div className="rectangle2">
-                    <img id="pic" src="https://serviced.co.il/wp-content/uploads/2022/01/%D7%A6%D7%95%D7%A8-%D7%A7%D7%A9%D7%A8-%D7%A9%D7%99%D7%A8%D7%95%D7%AA-%D7%9C%D7%A7%D7%95%D7%97%D7%95%D7%AA-%D7%99%D7%93-%D7%A9%D7%A8%D7%94.png" />
-
-                    <table id="tbl" border="1">
-                        <tr >
-                            <th> שם עמותה</th>
-                            <th> תחום</th>
-                            <th> שם רכז העמותה</th>
-                            <th> מייל הרכז</th>
-                            <th> פלאפון נייד</th>
-                            <th> טלפון נייח</th>
-                            <th>ת.ד.</th>
-                            <th>הערות</th>
-                        </tr>
-                        {/*  <tbody id="table-body"></tbody>  */}
-                        {/*  <td><button> ערוך</button></td> */}
-                        {/* <td button> מחק</button></td>  */}
-                        {/* <button> ערוך</button> */}
-                        {/* <button> מחק</button>  */}
-                    </table>
-                    <button id="add_contact">הוספת איש קשר</button>
-                </div>
-            </div>
-
+    <div>
+    <div className ="rectangle"></div>
+        <div className ="rectangle2">
+        <img id="pic" src="https://serviced.co.il/wp-content/uploads/2022/01/%D7%A6%D7%95%D7%A8-%D7%A7%D7%A9%D7%A8-%D7%A9%D7%99%D7%A8%D7%95%D7%AA-%D7%9C%D7%A7%D7%95%D7%97%D7%95%D7%AA-%D7%99%D7%93-%D7%A9%D7%A8%D7%94.png"/>
         </div>
-
+        
+        <div className ="search"/>
+            <form action="#">
+                <br/>
+                <select name="sdomain" id="sdomain">
+                <option value="" disabled selected>:תחום התנדבות</option>
+                <option value="1">לוגיסטיקה</option>
+            </select>
+            <br/>
+            <select name="sarea" id="sarea">
+                <option value="" disabled selected>:מחוז</option>
+                <option value="1">דרום</option>
+                <option value="2">ירושלים</option>
+                <option value="3">מרכז</option>
+                <option value="4">צפון</option>
+                <option value="5">שרון</option>
+        </select>
+        <br/>
+        <select name="scity" id="scity">
+                <option value="" disabled selected>:עיר</option>
+                <option value="0">ראשון לציון</option>
+                <option value="1">ירושלים</option>
+                <option value="2">אשדוד</option>
+                <option value="3">בית שמש</option>
+                <option value="4">תל אביב</option>
+    </select>
+    <br/> 
+    <br/>
+    <br/>
+            <button id="btn3" type="button">חפש</button>
+            
+    </form>
+        
+    </div>
     )
+
 }
--- client/src/components/User_name.jsx
+++ client/src/components/User_name.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom"
 export const User_name = (props) => {
 
 
-return(
-    <div>
-        <div className="rectangle"></div>
-    <div className="rectangle2">
-    <img id="pic" src="https://serviced.co.il/wp-content/uploads/2022/01/%D7%A6%D7%95%D7%A8-%D7%A7%D7%A9%D7%A8-%D7%A9%D7%99%D7%A8%D7%95%D7%AA-%D7%9C%D7%A7%D7%95%D7%97%D7%95%D7%AA-%D7%99%D7%93-%D7%A9%D7%A8%D7%94.png"/>
-    </div>
-    <div id="center">
-        <br/>
-        <h1 id="user">שלום שם משתמש</h1>
-        <br/>
-        <Link to='/search'> רשימת מתנדבים</Link>
-        <br/>
-        <br/>
-        <button type="button" id="btn2"> חיפוש מתנדבים</button>
-    </div>
-    </div>
+    return (
+        <div>
+            <div className="rectangle"></div>
+            <div className="rectangle2">
+                <img id="pic" src="https://serviced.co.il/wp-content/uploads/2022/01/%D7%A6%D7%95%D7%A8-%D7%A7%D7%A9%D7%A8-%D7%A9%D7%99%D7%A8%D7%95%D7%AA-%D7%9C%D7%A7%D7%95%D7%97%D7%95%D7%AA-%D7%99%D7%93-%D7%A9%D7%A8%D7%94.png" />
+            </div>
+            <div id="center">
+                <br />
+                <h1 id="user">שלום שם משתמש</h1>
+                <br />
+                <Link to='/search'>  <button type="button" id="btn2"> רשימת מתנדבים</button></Link>
+                <br />
+                <br />
+
+                <Link to='/search'>חיפוש מתנדבים </Link>
+
+            </div>
+        </div>
 
     )
 }
