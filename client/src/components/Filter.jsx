44be4b23bf7d263528905a23edbddac8e1f01d64
batzio <aniandata1234@gmail.com>
2022-06-09 20:15:31 +0300

    City names

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -11,7 +11,6 @@ export const Filter = (props) => {
     const [filter, setFilter] = useState('');
 
     async function getData() {
-        
         const dataArray = await getDocs(query(contactRef));
         dataArray.forEach(doc => {
             setCallData(prev => [...prev, doc.data()])
@@ -32,20 +31,22 @@ export const Filter = (props) => {
         )
     })
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        // send object to FirebaseError
-        await setDoc(doc(contactsRef), { name: nameValue,domain: domainValue,district:area, city:address, nameOfCoordinator: cnameValue, emailOfCoordinator: coEmail, cellPhone: cphoneValue, phone: phoneValue, po:POValue, notes: notes})
-        navigate("/list");
+
+
+    // const handleClick = async (e) => {
+    //     e.preventDefault();
+    //     // send object to FirebaseError
+    //     await setDoc(doc(contactsRef), { name: nameValue,domain: domainValue,district:area, city:address, nameOfCoordinator: cnameValue, emailOfCoordinator: coEmail, cellPhone: cphoneValue, phone: phoneValue, po:POValue, notes: notes})
+    //     navigate("/list");
         
-        {dataSearch.map((object, index) => (
-            <div className="req" key={index}>
-                <div>
-                    {object.source + " " + object.destination + " " + object.city + " " + object.date + " " + object.gender + " " + object.number_of_passengers + " " + object.car_type}
-                </div>
-            </div>
-        ))}
-    }
+    //     {dataSearch.map((object, index) => (
+    //         <div className="req" key={index}>
+    //             <div>
+    //                 {object.source + " " + object.destination + " " + object.city + " " + object.date + " " + object.gender + " " + object.number_of_passengers + " " + object.car_type}
+    //             </div>
+    //         </div>
+    //     ))}
+    // }
 
 
     // const handleSearch = () => {
@@ -83,22 +84,97 @@ export const Filter = (props) => {
                     <br />
                     <select name="scity" id="scity">
                     <option value="" disabled selected>בחר עיר</option>
-                        <option value="0">ראשון לציון</option>
-                        <option value="1">ירושלים</option>
-                        <option value="2">אשדוד</option>
-                        <option value="3">בית שמש</option>
-                        <option value="4">תל אביב</option>
+                    <option value="0">אום אל פחם</option>
+                    <option value="1">אופקים</option>
+                    <option value="2">אור יהודה</option>
+                    <option value="3">אור עקיבא</option>
+                    <option value="4">אילת</option>
+                    <option value="5">אריאל</option>
+                    <option value="6">אשדוד</option>
+                    <option value="7">אשקלון</option>
+                    <option value="8">באר שבע</option>
+                    <option value="9">בית שאן</option>
+                    <option value="10">בית שמש</option>
+                    <option value="11">בת ים</option>
+                    <option value="12">גבעת שמואל</option>
+                    <option value="13">גבעתיים</option>
+                    <option value="14">דימונה</option>
+                    <option value="15">הוד השרון</option>
+                    <option value="16">הרצליה</option>
+                    <option value="17">חדרה </option>
+                    <option value="18">חולון</option>
+                    <option value="19">חיפה</option>
+                    <option value="20">טבריה</option>
+                    <option value="21">טייבה</option>
+                    <option value="22">טירה</option>
+                    <option value="23">טירת הכרמל</option>
+                    <option value="24">טמרה</option>
+                    <option value="25">יבנה</option>
+                    <option value="26">יהוד מונסון</option>
+                    <option value="27">יקנעם</option>
+                    <option value="28">ירושלים</option>
+                    <option value="29">כפר יונה</option>
+                    <option value="30">כפר סבא</option>
+                    <option value="31">כפר קאסם</option>
+                    <option value="32">כרמיאל</option>
+                    <option value="33">לוד</option>
+                    <option value="34">מגדל העמק</option>
+                    <option value="35">מודיעין מכבים רעות</option>
+                    <option value="36">מעלה אדומים</option>
+                    <option value="37">מעלות תרשיחא</option>
+                    <option value="38">נהריה</option>
+                    <option value="39">נוף הגליל</option>
+                    <option value="40">נס ציונה</option>
+                    <option value="41">נצרת</option>
+                    <option value="42">נשר</option>
+                    <option value="43">נתיבות</option>
+                    <option value="44">נתניה</option>
+                    <option value="45">סח'נין</option>
+                    <option value="46">עכו</option>
+                    <option value="47">עפולה</option>
+                    <option value="48">עראבה</option>
+                    <option value="49">ערד</option>
+                    <option value="50">פתח תקווה</option>
+                    <option value="51">צפת</option>
+                    <option value="52">קלנסווה</option>
+                    <option value="53">קריית אונו</option>
+                    <option value="54">קריית אתא</option>
+                    <option value="55">קריית ביאליק</option>
+                    <option value="56">קריית גת</option>
+                    <option value="57">קריית ים</option>
+                    <option value="58">קריית מוצקין</option>
+                    <option value="59">קריית מלאכי</option>
+                    <option value="60">קריית שמונה</option>
+                    <option value="61">ראש העין</option>
+                    <option value="62">ראשון לציון</option>
+                    <option value="63">רהט</option>
+                    <option value="64">רחובות</option>
+                    <option value="65">רמלה</option>
+                    <option value="66">רמת גן </option>
+                    <option value="67">רמת השרון</option>
+                    <option value="68">רעננה</option>
+                    <option value="69">שדרות</option>
+                    <option value="70">שפרעם</option>
+                    <option value="71">תל אביב</option>
                     </select>
                     
                     <br />
                     <br />
                     <br />
-                     <button id="btnOK" type="button" onClick={handleClick}>חפש</button> 
-                
+                     <button id="btnOK" type="button" >חפש</button>  {/* onClick={handleClick} */}
 
                 </form>
-            </div>
 
+                {/* {dataSearch.map((object, index) => (
+                <div className="req" key={index}>
+                    <div>
+                        {object.source + " " + object.destination + " " + object.city + " " + object.date + " " + object.gender + " " + object.number_of_passengers + " " + object.car_type}
+                    </div> */}
+                    {/* <div>{object.source}</div> */}
+                {/* </div>
+            ))} */}
+
+            </div>
         </div>
     )
 
